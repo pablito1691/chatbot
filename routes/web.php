07e2bac4dc5fd975e085679dfe83477a27c1d67d
ivan8v <ivan8v@gmail.com>
2017-11-14 14:43:27 -0300

    Agregue la ruta de la api Agregue Vehiculo al modelo

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/vehiculos', function () {
+    return view('vehiculos')->with('vehiculos', \App\Vehiculo::all());
+});
