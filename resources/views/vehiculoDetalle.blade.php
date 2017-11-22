e722a3bb99b8b3a49ddd8426c2be9d52cd0df424
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 11:27:26 -0300

    active pagina 2

--- resources/views/vehiculoDetalle.blade.php
+++ resources/views/vehiculoDetalle.blade.php
@@ -10,7 +10,7 @@
 
                 @foreach($vehiculo->imagenes as $imagenes)
                     <div class="list-group">
-                        <a href=""><img width="200" height="200" src="{{asset('storage/img/'.$imagenes->ruta)}}" alt=""></a>
+                        <a href=""><img width="100" height="100" src="{{asset('storage/img/'.$imagenes->ruta)}}" alt=""></a>
                         <p></p>
                     </div>
                  @endforeach
