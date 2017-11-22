8f5f9ab58318bdb5410a91976307f6df0ca086e5
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 14:34:15 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -3,7 +3,7 @@
 
 <body>
 {{--<figure class="gallery-image-container thumbgallery_default-0">--}}
-    <img src="img/auto1.jpg"
+    <img src="/img/auto1.jpg"
          width="500" height="394"/>
 {{--</figure>--}}
 </body>
