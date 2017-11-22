647abb7c78848e420964966ff3c1def9040c59cf
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 14:33:05 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -1,18 +1,5 @@
 <html>
-<head>
-    <!-- Copiar dentro del tag HEAD -->
-    <script type="text/javascript">
-        <!--
-        function CargarFoto(img, ancho, alto) {
-            derecha = (screen.width - ancho) / 2;
-            arriba = (screen.height - alto) / 2;
-            string = "toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=" + ancho + ",height=" + alto + ",left=" + derecha + ",top=" + arriba + "";
-            fin = window.open(img, _blank, string, true);
-        }
 
-        // -->
-    </script>
-</head>
 
 <body>
 <figure class="gallery-image-container thumbgallery_default-0">
