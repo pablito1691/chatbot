1ef89f7fe8f653cf681cba1afeb487ab486b5559
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 14:14:01 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -1,5 +1,3 @@
-JavaScript
-
 <html>
 <head>
     <!-- Copiar dentro del tag HEAD -->
@@ -19,47 +17,5 @@ JavaScript
 <!-- Copiar dentro del tag BODY -->
 <!-- imagen.jpg es el nombre del archivo, 121 es el ancho de la imagen y 168 el alto -->
 <a href="javascript:CargarFoto('img/auto1.jpg','121','168')">Abrir imagen</a>
-</body>
-</html>
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-<html>
-<head>
-    <!-- Copiar dentro del tag HEAD -->
-    <script type="text/javascript">
-        <!--
-        function CargarFoto(img, ancho, alto){
-            derecha=(screen.width-ancho)/2;
-            arriba=(screen.height-alto)/2;
-            string="toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width="+ancho+",height="+alto+",left="+derecha+",top="+arriba+"";
-            fin=window.open(img,"",string);
-        }
-        // -->
-    </script>
-</head>
-</head>
-<body>
-<!-- Copiar dentro del tag BODY -->
-<!-- imagen.jpg es el nombre del archivo, 121 es el ancho de la imagen y 168 el alto -->
-<a href="javascript:CargarFoto('ejemplos/bisco.gif','121','168')">Abrir imagen</a>
 </body>
 </html>
