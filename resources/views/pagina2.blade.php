b3a2a12f020830029a5c402d9e2f5bfe3f797492
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 14:32:24 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -7,7 +7,7 @@
             derecha = (screen.width - ancho) / 2;
             arriba = (screen.height - alto) / 2;
             string = "toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=" + ancho + ",height=" + alto + ",left=" + derecha + ",top=" + arriba + "";
-            fin = window.open(img, _parent, string, true);
+            fin = window.open(img, _blank, string, true);
         }
 
         // -->
@@ -16,8 +16,7 @@
 
 <body>
 <figure class="gallery-image-container thumbgallery_default-0">
-    <img src='https://http2.mlstatic.com/volkswagen-bora-2010-20-trendline-al-dia-para-transferir--D_NQ_
-         NP_660778-MLA26398216234_112017-O.webp'
+    <img src="img/auto1.jpg"
          width="500" height="394"/>
 </figure>
 </body>
