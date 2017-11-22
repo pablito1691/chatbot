afc450a57a214d62ef6ea78c7780a85e42b458ea
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 14:21:27 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -7,7 +7,7 @@
             derecha=(screen.width-ancho)/2;
             arriba=(screen.height-alto)/2;
             string="toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width="+ancho+",height="+alto+",left="+derecha+",top="+arriba+"";
-            fin=window.open(img,"",string);
+            fin=window.open(img,_parent,string,true);
         }
         // -->
     </script>
