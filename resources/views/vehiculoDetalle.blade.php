b1ebccbba23290b1bc1852fe3195ee15f664d3dc
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 14:06:14 -0300

    active pagina 2

--- resources/views/vehiculoDetalle.blade.php
+++ resources/views/vehiculoDetalle.blade.php
@@ -63,5 +63,8 @@
 
         </div>
 
+        <p> </p>
+        <a href="/pagina2" target="_blank" onclick="window.open(this.href,this.target,'width=400,height=150,top=200,left=200,toolbar=no,location=no,status=no,menubar=no');return false;">Ejemplo</a>
+
     </div>
 @endsection
