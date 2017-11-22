cdee56d8d6f45a04e833028bbe335ae106ce5537
ivan8v <ivan8v@gmail.com>
2017-11-22 12:58:53 -0300

    Logo

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,26 @@ $(document).ready(function () {
         $('#imagen').attr('src',this.src);
     });
 
+    $('#modalConsulta').on('show.bs.modal', function (e) {
 
+        $modelo = $(e.relatedTarget).data('modelo');
+        $imagen = $(e.relatedTarget).data('imagen');
+
+
+        $(this).find('.modal-title').text('Consulta sobre '+$modelo);
+        $(this).find('#imgPopup').attr('src', $imagen);
+
+        $(this).find('#hiddenVehiculo').val($modelo);
+
+
+
+        form = $(e.relatedTarget).closest('form');
+
+        $(this).find('.modal-footer #confirmConsulta').data('form', form);
+
+
+
+    });
 
 
 
