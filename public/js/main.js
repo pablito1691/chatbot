c06576fc04a1898915250944aacc4378e5ac716e
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 12:51:16 -0300

    active pagina 2

--- public/js/main.js
+++ public/js/main.js
@@ -5,4 +5,14 @@ $(document).ready(function () {
     });
 
 
+    $('.imagen_mini').mouseover(function (e){
+        e.preventDefault();
+        $('.imagen_mini').on('click', function (e) {
+            e.preventDefault();
+            $('#imagen').attr('src',this.src);
+        });
+
+    });
+
+
 });
