99817052149f3c3f58e4f7e9b14f7c33bfe9d58d
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 14:30:54 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -3,19 +3,22 @@
     <!-- Copiar dentro del tag HEAD -->
     <script type="text/javascript">
         <!--
-        function CargarFoto(img, ancho, alto){
-            derecha=(screen.width-ancho)/2;
-            arriba=(screen.height-alto)/2;
-            string="toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width="+ancho+",height="+alto+",left="+derecha+",top="+arriba+"";
-            fin=window.open(img,_parent,string,true);
+        function CargarFoto(img, ancho, alto) {
+            derecha = (screen.width - ancho) / 2;
+            arriba = (screen.height - alto) / 2;
+            string = "toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=" + ancho + ",height=" + alto + ",left=" + derecha + ",top=" + arriba + "";
+            fin = window.open(img, _parent, string, true);
         }
+
         // -->
     </script>
 </head>
-</head>
+
 <body>
-<!-- Copiar dentro del tag BODY -->
-<!-- imagen.jpg es el nombre del archivo, 121 es el ancho de la imagen y 168 el alto -->
-<a href="javascript:CargarFoto('img/auto1.jpg','121','168')">Abrir imagen</a>
+<figure class="gallery-image-container thumbgallery_default-0">
+    <img src='https://http2.mlstatic.com/volkswagen-bora-2010-20-trendline-al-dia-para-transferir--D_NQ_
+         NP_660778-MLA26398216234_112017-O.webp'
+         width="500" height="394"/>
+</figure>
 </body>
 </html>
