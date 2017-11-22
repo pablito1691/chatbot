8b066b174cd22bd14fd02e0bd2e66c98736aa2bf
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-22 14:44:58 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -2,9 +2,12 @@
 
 
 <body>
-{{--<figure class="gallery-image-container thumbgallery_default-0">--}}
-    <img src="/img/auto1.jpg"
-         width="500" height="394"/>
-{{--</figure>--}}
+<script>
+    function newPopup(url) {
+        popupWindow = window.open(
+            url,'popUpWindow','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=yes')
+    }
+</script>
+<a href="javascript:void(0);" onClick="newPopup('https://www.cs.washington.edu/education/courses/cse455/12au/projects/project1/students/chantalm/artifact/images/flowers.jpg');">ScreenShot</a>
 </body>
 </html>
