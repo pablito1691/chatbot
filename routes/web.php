4ecf8711ce2e5cacab388f08a8950be8cab78241
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-23 08:59:54 -0300

    active pagina 2

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('/admin', function () {
 })->middleware('auth');
 
 Route::get('/pagina2', function(){
-    return view('pagina2');
+    return view('pagina2')->with('vehiculos',Vehiculo::all());
 });
 
 
