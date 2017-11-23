a1cabf0e39e9d8b358a72b36c9381bd839f954b2
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-23 14:07:52 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -30,8 +30,11 @@
 
             @foreach($vehiculo->imagenes as $imagenes)
                 <div class="list-group">
-                    <a href="#"><img onmouseover="hacerClick()" class="imagen_mini" width="100" height="100" src="{{asset('storage/img/'.$imagenes->ruta)}}"
-                                     alt="No se pudo cargar la imagen"></a>
+                    {{--<a href="#"><img onmouseover="hacerClick()" class="imagen_mini" width="100" height="100" src="{{asset('storage/img/'.$imagenes->ruta)}}"--}}
+                                     {{--alt="No se pudo cargar la imagen"></a>--}}
+                    <a href='img/bigimage03.jpg' class='cloud-zoom-gallery' title='Thumbnail 1'
+                       rel="useZoom: 'zoom3', smallImage: 'img/smallimage-3.jpg' ">
+                        <img src="img/tinyimage-3.jpg" alt = "Thumbnail 1"/></a>
                     <p></p>
                 </div>
             @endforeach
