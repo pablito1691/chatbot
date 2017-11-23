3494d4e7344816bc0af977d1185c2c5727e21b72
pablito1691 <33630263+pablito1691@users.noreply.github.com>
2017-11-23 12:22:49 -0300

    active pagina 2

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -1,120 +1,124 @@
 @extends('layouts.app')
 
 @section('content')
-    <script src="jquery-1.7.2.min.js"></script>
-    <script type="text/javascript" src="jquery.mlens-1.2.min.js"></script>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
 
+    <!-- Load the Cloud Zoom CSS file -->
+    <link href="/styles/cloud-zoom.css" rel="stylesheet" type="text/css" />
 
+    <!-- You can load the jQuery library from the Google Content Network.
+    Probably better than from your own server. -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 
-    <div class="container">
+    <!-- Load the Cloud Zoom JavaScript file -->
+    <script type="text/JavaScript" src="/js/cloud-zoom.1.0.2.js"></script>
 
-        <div class="row">
+</head>
+<body>
+<div class="row">
 
-            <div class="col-lg-3">
-                <h1 class="my-4">Fotos</h1>
+    <div class="col-lg-3">
+        <h1 class="my-4">Fotos</h1>
 
-                {{--@foreach($vehiculo->imagenes as $imagenes)--}}
-                    {{--<div class="list-group">--}}
-                        {{--<a href="#"><img onmouseover="hacerClick()" class="imagen_mini" width="100" height="100" src="{{asset('storage/img/'.$imagenes->ruta)}}"--}}
-                                         {{--alt="No se pudo cargar la imagen"></a>--}}
-                        {{--<p></p>--}}
-                    {{--</div>--}}
-                {{--@endforeach--}}
-            {{--</div>--}}
-
-
-            <!-- /.col-lg-3 -->
-
-            <div class="col-lg-9">
-
-                {{--<div class="card mt-4">--}}
-                    {{--<img width="900" height="400" id="imagen" class="card-img-top img-fluid" src="{{asset('storage/img/'.$vehiculo->imagen_mini)}}"--}}
-                         {{--alt="No se pudo cargar la imagen">--}}
-                    {{--<div class="card-body">--}}
-                        {{--<h3 class="card-title">{{$vehiculo->version->descripcion}}</h3>--}}
-                        {{--<h4>{{$vehiculo->precio}}</h4>--}}
-                        {{--<p class="card-text">{{$vehiculo->descripcion}}</p>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-                <!-- /.card -->
-
-            {{--<div class="card card-outline-secondary my-4">--}}
-            {{--<div class="card-header">--}}
-            {{--Reseñas--}}
-            {{--</div>--}}
-            {{--<div class="card-body">--}}
-            {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,--}}
-            {{--similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat--}}
-            {{--laborum. Sequi mollitia, necessitatibus quae sint natus.</p>--}}
-            {{--<small class="text-muted">Posted by Anonymous on 3/1/17</small>--}}
-            {{--<hr>--}}
-            {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,--}}
-            {{--similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat--}}
-            {{--laborum. Sequi mollitia, necessitatibus quae sint natus.</p>--}}
-            {{--<small class="text-muted">Posted by Anonymous on 3/1/17</small>--}}
-            {{--<hr>--}}
-            {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,--}}
-            {{--similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat--}}
-            {{--laborum. Sequi mollitia, necessitatibus quae sint natus.</p>--}}
-            {{--<small class="text-muted">Posted by Anonymous on 3/1/17</small>--}}
-            {{--<hr>--}}
-            {{--<a href="#" class="btn btn-success">Leave a Review</a>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-            <!-- /.card -->
-
+        @foreach($vehiculo->imagenes as $imagenes)
+            <div class="list-group">
+                <a href="#"><img onmouseover="hacerClick()" class="imagen_mini" width="100" height="100" src="{{asset('storage/img/'.$imagenes->ruta)}}"
+                                 alt="No se pudo cargar la imagen"></a>
+                <p></p>
             </div>
-            <!-- /.col-lg-9 -->
-
-        </div>
-
-        <p> </p>
-        <a href="/" target="_parent" onclick="window.open(this.href,this.target,'width=400,height=150,top=200,left=200,toolbar=no,location=no,status=no,menubar=no');return false;">Ejemplo</a>
-
-        <div id="contenido">
-            <img id="img_zoom" src="img/auto1.jpg" alt="Aca esta el auto con zoom" data-big="img/chery_grande.jpg" data-overlay="img/fondo_negro.png" />
-        </div>
-
-
-
-
+        @endforeach
     </div>
 
 
-    <script type="text/javascript">
-        $(document).ready(function()
-        {
-            $("#botella").mlens(
-                {
-                    imgSrc: $("#botella").attr("data-big"),   // path of the hi-res version of the image
-                    lensShape: "circle",                // shape of the lens (circle/square)
-                    lensSize: 180,                  // size of the lens (in px)
-                    borderSize: 4,                  // size of the lens border (in px)
-                    borderColor: "#fff",                // color of the lens border (#hex)
-                    borderRadius: 0,                // border radius (optional, only if the shape is square)
-                    imgOverlay: $("#botella").attr("data-overlay"), // path of the overlay image (optional)
-                    overlayAdapt: true // true if the overlay image has to adapt to the lens size (true/false)
-                });
-        });
-    </script>
+    <!-- /.col-lg-3 -->
+
+    <div class="col-lg-9">
+
+        <div class="card mt-4">
+            <img width="900" height="400" id="imagen" class="card-img-top img-fluid" src="{{asset('storage/img/'.$vehiculo->imagen_mini)}}"
+                 alt="No se pudo cargar la imagen">
+            <div class="card-body">
+                <h3 class="card-title">{{$vehiculo->version->descripcion}}</h3>
+                <h4>{{$vehiculo->precio}}</h4>
+                <p class="card-text">{{$vehiculo->descripcion}}</p>
+            </div>
+        </div>
+
+<a href='img/bigimage00.jpg' class = 'cloud-zoom' id='zoom1'
+   rel="adjustX: 10, adjustY:-4, softFocus:true">
+    <img src="img/smallimage-1.jpg" alt='' align="left" title="Zoom del auto" />
+</a>
 
 
 
-    {{--<php--}}
-            {{--#contenido {--}}
-            {{--position: relative;--}}
-            {{--width: 640px;--}}
-            {{--height: auto;--}}
-            {{--margin: 20px auto;--}}
-            {{--border: 12px solid #fff;--}}
-            {{--border-radius: 10px;--}}
-            {{--box-shadow: 1px 1px 5px rgba(50,50,50 0.5);--}}
-            {{--}--}}
-    {{--?/>--}}
+<p>Seleccione la foto para hacerle alto zoom</p>
 
-    
-    <img src="public/img/auto1.jpg" alt="No se puede cargar la foto">
-    </div>
+<!--
+        You can optionally create a gallery by creating anchors with a class of 'cloud-zoom-gallery'.
+        The anchor's href should point to the big zoom image.
+        In the rel attribute you must specify the id of the zoom to use (useZoom: 'zoom1'),
+        and also the small image to use (smallImage: /images/....)
+        -->
+
+<a href='img/bigimage00.jpg' class='cloud-zoom-gallery' title='Thumbnail 1'
+   rel="useZoom: 'zoom1', smallImage: 'img/smallimage-1.jpg' ">
+    <img src="img/tinyimage.jpg" alt = "Thumbnail 1"/></a>
+
+<a href='img/bigimage01.jpg' class='cloud-zoom-gallery' title='Thumbnail 2'
+   rel="useZoom: 'zoom1', smallImage: ' img/smallimage-2.jpg'">
+    <img src="img/tinyimage-1.jpg" alt = "Thumbnail 2"/></a>
+
+<a href='img/bigimage02.jpg' class='cloud-zoom-gallery' title='Thumbnail 3'
+   rel="useZoom: 'zoom1', smallImage: 'img/smallimage-3.jpg' ">
+    <img src="img/tinyimage-2.jpg" alt = "Thumbnail 3"/></a>
+
+</body>
+
+<style>
+
+    /* This is the moving lens square underneath the mouse pointer. */
+    .cloud-zoom-lens {
+        border: 4px solid #888;
+        margin:-4px;	/* Set this to minus the border thickness. */
+        background-color:#fff;
+        cursor:move;
+    }
+
+    /* This is for the title text. */
+    .cloud-zoom-title {
+        font-family:Arial, Helvetica, sans-serif;
+        position:absolute !important;
+        background-color:#000;
+        color:#fff;
+        padding:3px;
+        width:100%;
+        text-align:center;
+        font-weight:bold;
+        font-size:10px;
+        top:0px;
+    }
+
+    /* This is the zoom window. */
+    .cloud-zoom-big {
+        border:4px solid #ccc;
+        overflow:hidden;
+    }
+
+    /* This is the loading message. */
+    .cloud-zoom-loading {
+        color:white;
+        background:#222;
+        padding:3px;
+        border:1px solid #000;
+    }
+
+
+
+</style>
+
+
+
+</html>
 @endsection
-
-
--- resources/views/vehiculoDetalle.blade.php
+++ resources/views/vehiculoDetalle.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
 
+    <!-- Load the Cloud Zoom CSS file -->
+    <link href="/styles/cloud-zoom.css" rel="stylesheet" type="text/css" />
+
+    <!-- You can load the jQuery library from the Google Content Network.
+    Probably better than from your own server. -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
+    <!-- Load the Cloud Zoom JavaScript file -->
+    <script type="text/JavaScript" src="/js/cloud-zoom.1.0.2.js"></script>
+
+</head>
 @section('content')
+
+
     <div class="container">
 
         <div class="row">
