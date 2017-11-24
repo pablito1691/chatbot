aaad28ec1def61652abf618161c9870c6cab1ccf
ivan8v <ivan8v@gmail.com>
2017-11-24 08:27:55 -0300

    layout pagina2 formulario de contacto

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 
     <!-- Load the Cloud Zoom CSS file -->
-    <link href="/styles/cloud-zoom.css" rel="stylesheet" type="text/css" />
+    <link href="/styles/cloud-zoom.css" rel="stylesheet" type="text/css"/>
 
     <!-- You can load the jQuery library from the Google Content Network.
     Probably better than from your own server. -->
@@ -32,30 +33,35 @@
                     <div class="container-fluid">
                         <div class="row">
 
-                            <div class="col-lg-4">
+                            <div class="col-lg-2">
                                 <h1 class="my-4">Fotos</h1>
 
                                 @foreach($vehiculo->imagenes as $imagenes)
                                     <div class="list-group">
                                         {{--<a href="#"><img onmouseover="hacerClick()" class="imagen_mini" width="100" height="100" src="{{asset('storage/img/'.$imagenes->ruta)}}"--}}
                                         {{--alt="No se pudo cargar la imagen"></a>--}}
-                                        <a href='{{asset('storage/img/'.$imagenes->ruta)}}' class='cloud-zoom-gallery' title='Thumbnail 1'
+                                        <a href='{{asset('storage/img/'.$imagenes->ruta)}}' class='cloud-zoom-gallery'
+                                           title='Thumbnail 1'
                                            rel="useZoom: 'zoom1', smallImage: '{{asset('storage/img/'.$imagenes->ruta)}}' ">
-                                            <img onmouseover="hacerClick()" class="imagen_mini" width="100" height="100" src="{{asset('storage/img/'.$imagenes->ruta)}}" alt = "Thumbnail 1"/></a>
+                                            <img onmouseover="hacerClick()" class="imagen_mini" width="100" height="100"
+                                                 src="{{asset('storage/img/'.$imagenes->ruta)}}" alt="Thumbnail 1"/></a>
                                         <p></p>
                                     </div>
                                 @endforeach
                             </div>
 
-                            <div class="col-lg-8">
+                            <div class="col-lg-6">
 
-            <div class="card mt-4">
-                {{--<img width="500" height="300" id="imagen" class="card-img-top img-fluid" src="{{asset('storage/img/'.$vehiculo->imagen_mini)}}"--}}
-                     {{--alt="No se pudo cargar la imagen">--}}
-                <a href='{{asset('storage/img/'.$vehiculo->imagen_mini)}}' class = 'cloud-zoom' id='zoom1'
-                   rel="adjustX: 10, adjustY:-4, softFocus:true">
-                    <img width="500" height="300" id="imagen" src="{{asset('storage/img/'.$vehiculo->imagen_mini)}}" class="card-img-top img-fluid"  alt='' align="left" title="Zoom del auto" />
-                </a>
+                                <div class="card">
+                                    {{--<img width="500" height="300" id="imagen" class="card-img-top img-fluid" src="{{asset('storage/img/'.$vehiculo->imagen_mini)}}"--}}
+                                    {{--alt="No se pudo cargar la imagen">--}}
+                                    <a href='{{asset('storage/img/'.$vehiculo->imagen_mini)}}' class='cloud-zoom'
+                                       id='zoom1'
+                                       rel="adjustX: 10, adjustY:-4, softFocus:true">
+                                        <img width="500" height="300" id="imagen"
+                                             src="{{asset('storage/img/'.$vehiculo->imagen_mini)}}"
+                                             class="card-img-top img-fluid" alt='' align="left" title="Zoom del auto"/>
+                                    </a>
 
                                     <div class="card-body">
                                         <h3 class="card-title">{{$vehiculo->version->descripcion}}</h3>
@@ -65,10 +71,43 @@
                                 </div>
 
                             </div>
+                            <div class="col-lg-4">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4>Formulario de contacto</h4>
+                                    </div>
+                                    <div class="card-block">
+                                        <form id="consultaForm" action="{{url('admin/consultas')}}" method="POST" enctype="multipart/form-data">
+                                            <div class="form-group" id="formNombre">
+                                                <input class="form-control" type="text" name="nombre" id="inputNombre" placeholder="Nombre*">
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="form-group" id="formEmail">
+                                                    <input class="form-control" type="email" name="email" id="inputEmail" placeholder="Email*">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="form-group" id="formTelefono">
+                                                    <input class="form-control" type="text" name="telefono" id="inputTelefono" placeholder="Telefono*">
+                                                </div>
+                                            </div>
+                                            <div class="form-group" id="formMensaje">
+                                        <textarea name="mensaje" id="textMensaje" class="form-control"
+                                                  maxlength="499"rows="4" placeholder="Mensaje*"></textarea>
+                                            </div>
+
+                                            <input type="hidden" id="hiddenVehiculo" name="vehiculo">
+
+                                            {{ csrf_field() }}
+                                            <button class="btn btn-primary btn-block">Enviar tu consulta</button>
+                                        </form>
+                                    </div>
+
+                                </div>
+                            </div>
 
                         </div>
                     </div>
-
 
 
                 </div>
@@ -77,16 +116,10 @@
         </div>
 
 
-
-
-
-
     </div>
 
 
-
 </div>
-
 
 
 </body>
@@ -96,43 +129,41 @@
     /* This is the moving lens square underneath the mouse pointer. */
     .cloud-zoom-lens {
         border: 4px solid #888;
-        margin:-4px;	/* Set this to minus the border thickness. */
-        background-color:#fff;
-        cursor:move;
+        margin: -4px; /* Set this to minus the border thickness. */
+        background-color: #fff;
+        cursor: move;
     }
 
     /* This is for the title text. */
     .cloud-zoom-title {
-        font-family:Arial, Helvetica, sans-serif;
-        position:absolute !important;
-        background-color:#000;
-        color:#fff;
-        padding:3px;
-        width:100%;
-        text-align:center;
-        font-weight:bold;
-        font-size:10px;
-        top:0px;
+        font-family: Arial, Helvetica, sans-serif;
+        position: absolute !important;
+        background-color: #000;
+        color: #fff;
+        padding: 3px;
+        width: 100%;
+        text-align: center;
+        font-weight: bold;
+        font-size: 10px;
+        top: 0px;
     }
 
     /* This is the zoom window. */
     .cloud-zoom-big {
-        border:4px solid #ccc;
-        overflow:hidden;
+        border: 4px solid #ccc;
+        overflow: hidden;
     }
 
     /* This is the loading message. */
     .cloud-zoom-loading {
-        color:white;
-        background:#222;
-        padding:3px;
-        border:1px solid #000;
+        color: white;
+        background: #222;
+        padding: 3px;
+        border: 1px solid #000;
     }
 
 
-
 </style>
-
 
 
 </html>
